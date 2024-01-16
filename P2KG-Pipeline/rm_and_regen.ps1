038969aad4ef12686a110fb63812f96e50d6467b
James McDermott <jamesmichaelmcdermott@gmail.com>
2024-01-16 12:33:15 +0000

    separate the rm and regenerate steps from start_local into its own file

---
+++ P2KG-Pipeline/rm_and_regen.ps1
@@ -0,0 +1,5 @@
+
+# Command line for testing. Remove all the ttl and regenerate.
+
+rm JAMS\*\*\*.jams; rm ..\patterns-knowledge-graph-datasets\RDF\*\*\*.ttl; python .\pattern2kg_pipeline.py
+
--- P2KG-Pipeline/start_local.ps1
+++ P2KG-Pipeline/start_local.ps1
@@ -1,9 +1,4 @@
 
-# Command line for testing. Remove all the ttl and regenerate.
-
-rm JAMS\*\*\*.jams; rm ..\patterns-knowledge-graph-datasets\RDF\*\*\*.ttl; python .\pattern2kg_pipeline.py
-
-
 # drop db.
 
 # Note current namespace: kb
