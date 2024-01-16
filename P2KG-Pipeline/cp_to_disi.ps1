3b5de9a1e8d5e381319e6bd9454c086c401bf818
James McDermott <jamesmichaelmcdermott@gmail.com>
2024-01-16 22:40:25 +0000

    include an extra needed ttl file from TUNES

--- P2KG-Pipeline/cp_to_disi.ps1
+++ P2KG-Pipeline/cp_to_disi.ps1
@@ -1,10 +1,11 @@
 # cp to disi
 
-mkdir test/KG
+# mkdir test/KG
 cp D:\vc\tunes-knowledge-graph\kg\tunes-thesession-collectionconcepts.ttl test/KG
 cp D:\vc\tunes-knowledge-graph\kg\tunes-thesession-entities.ttl test/KG
 cp D:\vc\tunes-knowledge-graph\kg\tunes-thesession-formtypes.ttl test/KG
 cp D:\vc\tunes-knowledge-graph\kg\tunes-thesession-tunes.ttl test/KG
+cp D:\vc\tunes-knowledge-graph\kg\tunes-mtc-collectionconcepts.ttl test/KG
 cp D:\vc\patterns-knowledge-graph\patterns-knowledge-graph-datasets\RDF\mtc_ann_rdf_cat.ttl test/KG
 cp D:\vc\patterns-knowledge-graph\patterns-knowledge-graph-datasets\RDF\thesession_annotated_subset_rdf_cat.ttl test/KG
 
