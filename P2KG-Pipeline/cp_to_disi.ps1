e905e0b473a5a72046a20f51aef3df19eb848457
James McDermott <jamesmichaelmcdermott@gmail.com>
2024-01-16 22:00:47 +0000

    script for copying ttl to server - initial commit

---
+++ P2KG-Pipeline/cp_to_disi.ps1
@@ -0,0 +1,15 @@
+# cp to disi
+
+mkdir test/KG
+cp D:\vc\tunes-knowledge-graph\kg\tunes-thesession-collectionconcepts.ttl test/KG
+cp D:\vc\tunes-knowledge-graph\kg\tunes-thesession-entities.ttl test/KG
+cp D:\vc\tunes-knowledge-graph\kg\tunes-thesession-formtypes.ttl test/KG
+cp D:\vc\tunes-knowledge-graph\kg\tunes-thesession-tunes.ttl test/KG
+cp D:\vc\patterns-knowledge-graph\patterns-knowledge-graph-datasets\RDF\mtc_ann_rdf_cat.ttl test/KG
+cp D:\vc\patterns-knowledge-graph\patterns-knowledge-graph-datasets\RDF\thesession_annotated_subset_rdf_cat.ttl test/KG
+
+# zip KG
+# scp
+# unzip on dest
+# then another script over there
+# all my scripts "over there" should be in vc here
